04e4503bdb48b90759c35216d0b59cb3210a4bc7
Dylan Levy <djlevy26@gmail.com>
2026-09-23 18:58:02 -0400

    chore: add /api/create-link route

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # AU26 System Project - URL Shortener
 
 This is the monorepo home.
+
+NOTE: node is required to be installed for `wrangler` to respond to requests. 😡
--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -6,4 +6,14 @@ app.get('/:code', (c) => {
     return c.redirect('https://google.com/search?q=The%20Ohio%20State%20University', 302);
 })
 
+app.post("/api/create-link", async (c) => {
+    const body = await c.req.json<{ url: string }>();
+    if (!body) return c.status(400);
+    if (!body.url) return c.status(400);
+
+    const { url } = body;
+
+    console.log(url);
+});
+
 export default app
